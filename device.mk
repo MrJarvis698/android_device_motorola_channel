dceb2d7ba5a4674c789797de50cb3ddf295dc155
Deivid21 <david.parra.ignacio@gmail.com>
2020-09-27 00:22:48 +0000

    channel: exclude some packages

--- device.mk
+++ device.mk
@@ -32,6 +32,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     MotCamera2
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
